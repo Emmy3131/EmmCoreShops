d89502886d70307a84d1867d9adc77b07aa908f5
Emmy3131 <devinechild3003@gmail.com>
2026-08-09 21:09:10 +0100

    wip

--- src/pages/Admin/WithdrawalManagement.jsx
+++ src/pages/Admin/WithdrawalManagement.jsx
@@ -246,7 +246,7 @@ const WithdrawalManagement = () => {
       setProcessing(true);
 
       await api.patch(
-        `/withdrawals/${withdrawal._id}`,
+        `/withdrawals/${withdrawal._id}/aprove`,
         {
           status: "paid",
           adminNote:
